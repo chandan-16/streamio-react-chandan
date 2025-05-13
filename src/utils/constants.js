fc482d3a0ebe43065b8676ea9cbb961137e01a12
chandan-16 <chandangautam420.cg@gmail.com>
2025-05-13 18:02:07 +0530

    Added Constants and changes in package.json

---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=AIzaSyAy4Q97WPjxbJBnh_CIG8Jbue-XTda1tEg";
+
+export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
